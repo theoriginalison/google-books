9b90b27702bef8ad2b479d8c79848acdbf264c81
Alison Lubar <aslubar@gmail.com>
2021-02-01 20:39:56 -0500

    created card, nav, and search components

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+    return (
+        <Nav
+            activeKey="/home"
+            onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+            <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+    </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    )
+}
+
+export default Nav;
