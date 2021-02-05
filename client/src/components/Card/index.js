75276fd8ffe2510b971ff9f3fd7ea6c6293e818b
Alison Lubar <aslubar@gmail.com>
2021-02-04 22:00:24 -0500

    created a functional card

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 
+//get this from regular old bootstrap and not react bootstrap OR install react-bootstrap, then import react bootstrap here and get the Card
+
 function Card(props) {
     return (
 
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-    </Card.Text>
-            </Card.Body>
-        </Card>
+        <div className="card" style={{ width: '18rem' }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
 
     )
 }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,24 @@
 import React from "react";
 import Card from "../components/Card";
+import { Input, FormBtn } from "../components/Form";
+
 
 const Search = () => {
     return (
         <>
+            <form>
+                <Input
+                // onChange={handleInputChange}
+                // name="title"
+                // placeholder="Search for a book title..."
+                />
+                <FormBtn
+                // disabled={!(formObject.author && formObject.title)}
+                // onClick={handleFormSubmit}
+                >
+                    Find a Book
+              </FormBtn>
+            </form>
             <Card />
         </>
     )
